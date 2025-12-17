7d0cf25cfaa9de82e8c467ecbc449601766cd380
jnnjh <joannejhoieacar@gmail.com>
2025-12-18 00:10:16 +0100

    answered 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,24 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// ANSWER: The function will not return anything.
 
+// ---Original Code---
+/*
 function multiply(a, b) {
   console.log(a * b);
 }
-
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+*/
 
 // =============> write your explanation here
+// ANSWER: The function indeed didn't return anything that's why the value of the function multiply(a, b) is undefined. We should refactor line 7 to return something instead of only logging it to the console.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+// ---NEW CODE---
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,7 +2,10 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// ANSWER: In lines 13, 14, and 15, the function will have the same value(3).
 
+// ---Original Code---
+/*
 const num = 103;
 
 function getLastDigit() {
@@ -12,13 +15,30 @@ function getLastDigit() {
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+*/
 
 // Now run the code and compare the output to your prediction
 // =============> write the output here
+/*
+The last digit of 42 is 3
+The last digit of 105 is 3
+The last digit of 806 is 3
+*/
+
 // Explain why the output is the way it is
 // =============> write your explanation here
+// ANSWER: The value will always be the same because the function doesn't have any parameter to pass the argument. Even though in the examples, we put an argument in the function, only the num that is declared outside the function will be used.
+
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+// ---New Code---
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
