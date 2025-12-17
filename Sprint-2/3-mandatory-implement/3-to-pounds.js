df143d5f1a0818c1ed195f8d90a2b0e77ccd9742
jnnjh <joannejhoieacar@gmail.com>
2025-12-18 00:41:26 +0100

    answered 3-mandatory-implement

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,35 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+/*
+const penceString = "399p";
+
+const penceStringWithoutTrailingP = penceString.substring(
+  0,
+  penceString.length - 1
+);
+
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const pounds = paddedPenceNumberString.substring(
+  0,
+  paddedPenceNumberString.length - 2
+);
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+
+console.log(`£${pounds}.${pence}`);
+*/
+function toPounds(penceString){
+    const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
+    const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+    const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2);
+    const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+    return `£${pounds}.${pence}`;
+}
+
+console.log(toPounds("555p"));
+console.log(toPounds("123456p"));
+console.log(toPounds("56789p"));
+console.log(toPounds("100p"));
