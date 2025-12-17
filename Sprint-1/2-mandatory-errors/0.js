8fde25d5cf55e520a97ddf9ba0a958e287b172cf
jnnjh <joannejhoieacar@gmail.com>
2025-12-17 21:27:20 +0100

    answered 2-mandatory-errors

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,9 @@
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+/*
 This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+*/
+
+// ANSWER: if it's just one line of comment, use 2 slashes and /**/ for multiple lines.
--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -2,3 +2,8 @@
 
 const age = 33;
 age = age + 1;
+
+/*
+ANSWER: Error in reassignment of age variable. We used const so we can't reassign values to it.
+        To fix this, use let instead of const.
+*/
