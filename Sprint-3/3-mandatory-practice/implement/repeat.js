1d343be8b8c15461556a580a9ce1756530255c27
jnnjh <joannejhoieacar@gmail.com>
2025-12-22 21:40:47 +0100

    answered repeat

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,8 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+    if(count === 1) return str;
+    else if(count === 0) return "";
+    else if(count < 0) return "Invalid repeat count: must be 0 or greater.";
+    else return str.repeat(count);
 }
 
 module.exports = repeat;
